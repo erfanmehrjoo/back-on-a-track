9a234e3a07ecd864ef26b0c14501eee4204ca5a9
erfan <e.mehrjoo.e@gmail.com>
2023-09-29 13:37:30 +0330

    make the simple replacing app

---
+++ replacewordapp.py
@@ -0,0 +1,10 @@
+def replace_word():
+    
+    str = "Hi guys , i am erfan"
+    word_to_replace = input("Enter word to replace: ")
+    word_replacment = input("Enter word replacment: ")
+    print(str.replace(word_to_replace , word_replacment))
+    
+    
+    
+replace_word()
