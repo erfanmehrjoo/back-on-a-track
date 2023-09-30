f2cf9b090953f36682e1b03f82a003825c0ab841
erfan <e.mehrjoo.e@gmail.com>
2023-10-01 00:07:20 +0330

    simple qrcode gnerator

---
+++ qrcodeapp.py
@@ -0,0 +1,15 @@
+import qrcode
+
+def genrate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")  
+    
+genrate_qrcode("erfan mehrjoo")
